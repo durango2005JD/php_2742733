8bd3133a5062fdf159aa6e82d79079492588e6a1
ochoa <juandavidochoarivera9@gmail.com>
2024-05-14 15:54:55 -0500

    Variables actualizacion

---
+++ 01-variables-sintaxis/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$saludo = "Hola";
+$nombre1 = "Juan";
+$nombre2 = "Pepito";
+
+echo "$saludo $nombre1 y $nombre2 ¿Como estan? <br> <br> pagina creada por: <br> -$nombre1 <br> -$nombre2 <br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index_2.php">CLIC ACA</a>
+</body>
+</html> 
