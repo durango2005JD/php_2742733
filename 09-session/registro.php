e0def9145bcdf891b4aa54d9d2364b828f6e3b30
durango2005JD <juliandurango2005@gmail.com>
2024-05-28 15:33:33 -0500

    ejercicio inicio de sesion

--- 09-session/registro.php
+++ 09-session/registro.php
@@ -1,19 +1,31 @@
 <?php session_start();
 
-
  if ($_SERVER['REQUEST_METHOD'] == 'POST'){  
     $usuario = $_POST ['username'];
     $password = $_POST ['password'];
+    $email = $_POST ['email'];
 }
 
 if(empty($usuario) or empty($password)){
     echo 'rellene el formulario';
     }else{
-    //echo $usuario .' - '. $password;
+
     $_SESSION['userRegister'] = $usuario;
     $_SESSION['passRegister'] = $password;
-        //echo "Variables de sesion guardadas";
-        //header('Location: index.php');
+    $_SESSION['emailRegister'] = $email;
+    
+    try {
+        $conexion = new PDO("mysql: host=localhost; dbname=focaapp",'root','');
+        echo "conexion OK <br>";
+    } catch (PDOException $e) {
+        echo "Error:" . $e->getMessage();
+    } 
+    $statement = $conexion->prepare(" INSERT INTO `userapp` (`ID`, `Username`, `Email`, `Password`)
+    VALUES (NULL, :username, :email, :pass)" );
+
+    $statement->execute(array(":username"=>$usuario,":email"=>$email,":pass"=>$password));
+
+    $statement = $statement->fetchAll();
 }
 
     echo 'datos enviados';
@@ -37,7 +49,9 @@ if(empty($usuario) or empty($password)){
     <form action="registro.php" method="post">
 
     <label for="username">User</label>
-    <input id="username" type="text" placeholder="User" name="username">
+    <input id="username" type="text" placeholder="User" name="username"> <br>
+    <label for="email">Email</label>
+    <input type="email" id="email" placeholder="Email" name="email" required> <br>
     <label for="password">Password</label>
     <input id="password" type="password" placeholder="Password" name="password">
     <button type="submit">Registro</button>
@@ -46,7 +60,7 @@ if(empty($usuario) or empty($password)){
 
     <?php if( isset($_SESSION['userRegister'])): ?>
     <p>Datos registraos, ya puedes iniciar sesion</p>
-    <p><?php echo $_SESSION['userRegister'] . '-' . $_SESSION['passRegister']; ?></p>
+    <p><?php echo $_SESSION['userRegister'] . '<br>' . $_SESSION['passRegister'] . '<br>' . $_SESSION['emailRegister']; ?></p>
     <a href="index.php">Iniciar Sesion</a>
     <?php endif ?>    
 </body>
