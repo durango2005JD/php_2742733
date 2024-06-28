bbcc8ab41626f5e72c1bea6da50a56079069aea2
durango2005JD <148263835+durango2005JD@users.noreply.github.com>
2024-06-27 23:59:01 -0500

    Delete php_2742733-main directory

--- php_2742733-main/10-CRUD/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-try {
-    $conexion = new PDO("mysql: host=localhost; dbname=focaapp",'root','');
-} catch (PDOException $e) {
-    echo "Error:" . $e->getMessage();
-} 
-
-?>
